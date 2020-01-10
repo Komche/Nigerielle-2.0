5160bf890792ea8bdad42056217279231b39eb34
cherifatou <saidoumoussacherifa@gmail.com>
2020-01-10 14:03:14 +0100

    ajout des pages

---
+++ view/contact.php
@@ -0,0 +1,128 @@
+<?php
+    $title = "Accueil";
+    ob_start();
+?>
+<div class="hero" id="highlighted">
+      <div class="inner">
+        <!--Slideshow-->
+        <div  class="container">
+        <h2 class="h1 text-weight-light">
+                      <span class="text-primary">contactez-nous</span>
+                    </h2>
+                  <img src="public/img/n3.JPG" width="1500px" height="100" class="center-block img-responsive">
+        </div>
+      </div>
+  </div>
+   <!-- ======== @Region: #footer ======== -->
+
+            <div class="container">
+                <div class="row" id="contact">
+                    <div class="col-md-3">
+                        <address>
+                            
+                            <address>
+                            <strong>Adresse</strong><br>
+                            <i class="fa fa-map-pin fa-fw text-primary"></i> Niamey-Niger
+                            <br>
+                            <i class="fa fa-phone fa-fw text-primary"></i> + 227 90 95 63 64/ +227 93956364
+                            <br>
+                            <i class="fa fa-envelope-o fa-fw text-primary"></i> contact@nigerielle.ne
+                            <br>
+                        </address>
+                    </div>
+                    <div class="col-md-6">
+                        <h4 class="text-uppercase">
+                            contactez-nous
+                        </h4>
+                        <div class="form">
+                            <div id="sendmessage">votre message a été envoyé. Merci!</div>
+                            <div id="errormessage"></div>
+                            <form action="" method="post" role="form" class="contactForm">
+                                <div class="form-group">
+                                    <input type="text" name="nom" class="form-control" id="nom" placeholder="votre nom" data-rule="minlen:4" data-msg="entrer votre nom" />
+                                    <div class="validation"></div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control" name="email" id="email" placeholder="votre adresse email" data-rule="email" data-msg="entrer votre adresse mail" />
+                                    <div class="validation"></div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" name="objet" id="objet" placeholder="objet" data-rule="minlen:4" data-msg="entrer l'objet" />
+                                    <div class="validation"></div>
+                                </div>
+                                <div class="form-group">
+                                    <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                                    <div class="validation"></div>
+                                </div>
+                                <div class="text-center"><button type="submit">envoyer Message</button></div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <h4 class="text-uppercase">
+                          rejoingnez-nous:
+                        </h4>
+                        <!--social media icons-->
+                        <div class="social-media social-media-stacked">
+                            <!--@todo: replace with company social media details-->
+                            <a href="#"><i class="fa fa-twitter fa-fw"></i> Twitter</a>
+                            <a href="#"><i class="fa fa-facebook fa-fw"></i> Facebook</a>
+                            <a href="#"><i class="fa fa-google-plus fa-fw"></i> Google+</a>
+                        </div>
+                    </div>
+                </div>
+  <!-- ======== @Region: #content ======== -->
+  
+
+    </div>
+    <!-- Services -->
+    
+    <!--
+Background image callout with CSS overlay
+Usage: data-block-bg-img="IMAGE-URL" to apply a background image clearly via jQuery .block-bg-overlay = overlays the background image, colour is inherited from block-bg-* classes .block-bg-overlay-NUMBER = determines opcacity value of overlay from 1-9 (default is 5) ie. .block-bg-overlay-2 or .block-bg-overlay-6
+-->
+  
+   <!-- Call out block -->
+   <div class="block block-pd-sm block-bg-primary">
+    <div class="container">
+      <div class="row">
+        <h3 class="col-md-4">
+            Partenaires
+          </h3>
+        <div class="col-md-8">
+          <div class="row">
+            <!--Client logos should be within a 120px wide by 60px height image canvas-->
+            <div class="col-xs-6 col-md-2">
+              <a href="https://bootstrapmade.com" title="Client 1">
+                  <img src="public/img/sonibank.jpeg" height="1000px" width="150px"  alt="" class="img-responsive">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-2">
+              <a href="https://bootstrapmade.com" title="Client 2">
+                  <img src="public/img/seen.jpeg" alt="Client 2 logo" class="img-responsive">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-2">
+              <a href="https://bootstrapmade.com" title="Client 3">
+                  <img src="public/img/orano.png" alt="Client 3 logo" class="img-responsive">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-2">
+              <a href="https://bootstrapmade.com" title="Client 4">
+                  <img src="public/img/haut.jpg" alt="Client 4 logo" class="img-responsive">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-2">
+              <a href="https://bootstrapmade.com" title="Client 5">
+                  <img src="public/img/banque.png" alt="Client 5 logo" class="img-responsive">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
+    $content = ob_get_clean();
+    require('template.php');
+  ?>
---
+++ view/galerie.php
@@ -0,0 +1,221 @@
+<?php
+    $title = "Accueil";
+    ob_start();
+?>
+<div class="hero" id="highlighted">
+      <div class="inner">
+        <!--Slideshow-->
+        <div  class="container">
+        <h2 class="h1 text-weight-light">
+                      <span class="text-primary">Galerie photos</span>
+                    </h2>
+                  <img src="public/img/slide4.JPG" width="1500px" height="200" class="center-block img-responsive">
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ======== @Region: #content ======== -->
+  <div id="content">
+    <!-- Mission Statement -->
+    <div class="mission text-center block block-pd-sm block-bg-noise">
+      <div class="container">
+        <h2 class="text-shadow-white">
+        Créer un réseau de femmes entrepreneures, qui permettra l’échange de bonnes pratiques, d’expériences, et mutualiser les moyens et les compétences.
+          
+          </h2>
+      </div>
+    </div>
+    <!--Showcase-->
+    <div class="showcase block block-border-bottom-grey">
+      <div class="container">
+        <h2 class="block-title">
+          Galerie
+          </h2>
+
+        <div class="item-carousel" data-toggle="owlcarousel" data-owlcarousel-settings='{"items":4, "pagination":false, "navigation":true, "itemsScaleUp":true}'>
+          <div class="item">
+            <a href="#" class="overlay-wrapper">
+                <img src="public/img/n10.JPG" alt="Project 1 image" class="img-responsive underlay">
+                <span class="overlay">
+                  <span class="overlay-content"> <span class="h4">Actualités 1</span> </span>
+                </span>
+              </a>
+            <div class="item-details bg-noise">
+              <h4 class="item-title">
+                  <a href="#">Galerie</a>
+                </h4>
+             <!-- <a href="#" class="btn btn-more"><i class="fa fa-plus"></i>Read more</a>-->
+            </div>
+          </div>
+          <div class="item">
+            <a href="#" class="overlay-wrapper">
+                <img src="public/img/n14.jpg" alt="Project 2 image" class="img-responsive underlay">
+                <span class="overlay">
+                  <span class="overlay-content"> <span class="h4">Galerie</span> </span>
+                </span>
+              </a>
+            <div class="item-details bg-noise">
+              <h4 class="item-title">
+                  <a href="#">Galerie 2</a>
+                </h4>
+             <!-- <a href="#" class="btn btn-more"><i class="fa fa-plus"></i>Read more</a>-->
+            </div>
+          </div>
+          <div class="item">
+            <a href="#" class="overlay-wrapper">
+                <img src="public/img/n10.JPG" alt="Project 3 image" class="img-responsive underlay">
+                <span class="overlay">
+                  <span class="overlay-content"> <span class="h4">Galerie 3</span> </span>
+                </span>
+              </a>
+            <div class="item-details bg-noise">
+              <h4 class="item-title">
+                  <a href="#">Galerie 3</a>
+                </h4>
+              <!--<a href="#" class="btn btn-more"><i class="fa fa-plus"></i>Read more</a>-->
+            </div>
+          </div>
+          <div class="item">
+            <a href="#" class="overlay-wrapper">
+                <img src="public/img/slide3.JPG" alt="Project 4 image" class="img-responsive underlay">
+                <span class="overlay">
+                  <span class="overlay-content"> <span class="h4">Galerie 4</span> </span>
+                </span>
+              </a>
+            <div class="item-details bg-noise">
+              <h4 class="item-title">
+                  <a href="#">Galerie 4</a>
+                </h4>
+             <!-- <a href="#" class="btn btn-more"><i class="fa fa-plus"></i>Read more</a>-->
+            </div>
+          </div>
+          <div class="item">
+            <a href="#" class="overlay-wrapper">
+                <img src="public/img/slide.JPG" alt="Project 5 image" class="img-responsive underlay">
+                <span class="overlay">
+                  <span class="overlay-content"> <span class="h4">Galerie 5</span> </span>
+                </span>
+              </a>
+            <div class="item-details bg-noise">
+              <h4 class="item-title">
+                  <a href="#">Galerie 5</a>
+                </h4>
+              
+             <!-- <a href="#" class="btn btn-more"><i class="fa fa-plus"></i>Read more</a>-->
+            </div>
+          </div>
+          <div class="item">
+            <a href="#" class="overlay-wrapper">
+                <img src="public/img/n14.jpg" alt="Project 6 image" class="img-responsive underlay">
+                <span class="overlay">
+                  <span class="overlay-content"> <span class="h4">Galerie 6</span> </span>
+                </span>
+              </a>
+            <div class="item-details bg-noise">
+              <h4 class="item-title">
+                  <a href="#">Galerie 6</a>
+                </h4>
+             <!-- <a href="#" class="btn btn-more"><i class="fa fa-plus"></i>Read more</a>-->
+            </div>
+          </div>
+          <div class="item">
+            <a href="#" class="overlay-wrapper">
+                <img src="public/img/n14.jpg" alt="Project 7 image" class="img-responsive underlay">
+                <span class="overlay">
+                  <span class="overlay-content"> <span class="h4">Galerie 7</span> </span>
+                </span>
+              </a>
+            <div class="item-details bg-noise">
+              <h4 class="item-title">
+                  <a href="#">Galerie 7</a>
+                </h4>
+             <!-- <a href="#" class="btn btn-more"><i class="fa fa-plus"></i>Read more</a>-->
+            </div>
+          </div>
+          <div class="item">
+            <a href="#" class="overlay-wrapper">
+                <img src="public/img/slide4.JPG" alt="Project 8 image" class="img-responsive underlay">
+                <span class="overlay">
+                  <span class="overlay-content"> <span class="h4">Galerie 8</span> </span>
+                </span>
+              </a>
+            <div class="item-details bg-noise">
+              <h4 class="item-title">
+                  <a href="#">Galerie 8</a>
+                </h4>
+             <!-- <a href="#" class="btn btn-more"><i class="fa fa-plus"></i>Read more</a>-->
+            </div>
+          </div>
+          <div class="item">
+            <a href="#" class="overlay-wrapper">
+                <img src="public/img/slide5.JPG" alt="Project 9 image" class="img-responsive underlay">
+                <span class="overlay">
+                  <span class="overlay-content"> <span class="h4">Galerie 9</span> </span>
+                </span>
+              </a>
+            <div class="item-details bg-noise">
+              <h4 class="item-title">
+                  <a href="#">Galerie 9</a>
+                </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Services -->
+    
+    <!--
+Background image callout with CSS overlay
+Usage: data-block-bg-img="IMAGE-URL" to apply a background image clearly via jQuery .block-bg-overlay = overlays the background image, colour is inherited from block-bg-* classes .block-bg-overlay-NUMBER = determines opcacity value of overlay from 1-9 (default is 5) ie. .block-bg-overlay-2 or .block-bg-overlay-6
+-->
+  <h1 class="text-center">Retrouvez-nous <h1><br>
+                </h3>
+                <div class="row">
+                    <div style="background-color: #741b47; padding: 20px" class="col-lg-12">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3878.8807384598886!2d2.0901718497753445!3d13.542906805703378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x11da9f741f03c03f%3A0xb7df0e3b0a2a5aac!2sNigerielles!5e0!3m2!1sfr!2sne!4v1568832175108!5m2!1sfr!2sne" style="width: 100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                    </div>
+                </div>
+   <!-- Call out block -->
+   <div class="block block-pd-sm block-bg-primary">
+    <div class="container">
+      <div class="row">
+        <h3 class="col-md-4">
+            Partenaires
+          </h3>
+        <div class="col-md-8">
+          <div class="row">
+            <!--Client logos should be within a 120px wide by 60px height image canvas-->
+            <div class="col-xs-6 col-md-2">
+              <a href="https://bootstrapmade.com" title="Client 1">
+                  <img src="public/img/sonibank.jpeg" height="1000px" width="150px"  alt="" class="img-responsive">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-2">
+              <a href="https://bootstrapmade.com" title="Client 2">
+                  <img src="public/img/seen.jpeg" alt="Client 2 logo" class="img-responsive">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-2">
+              <a href="https://bootstrapmade.com" title="Client 3">
+                  <img src="public/img/orano.png" alt="Client 3 logo" class="img-responsive">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-2">
+              <a href="https://bootstrapmade.com" title="Client 4">
+                  <img src="public/img/haut.jpg" alt="Client 4 logo" class="img-responsive">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-2">
+              <a href="https://bootstrapmade.com" title="Client 5">
+                  <img src="public/img/banque.png" alt="Client 5 logo" class="img-responsive">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
+    $content = ob_get_clean();
+    require('template.php');
+  ?>
