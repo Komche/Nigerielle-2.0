76cf08d6217069ebcc49675728be4b80dae2b704
cherifatou <saidoumoussacherifa@gmail.com>
2020-02-05 10:35:26 +0100

    modification d'une image

--- view/engagement_view.php
+++ view/engagement_view.php
@@ -19,7 +19,7 @@
               </h3>
             <img src="public/img/n14.jpg" alt="About us" class="img-responsive img-thumbnail pull-right m-l m-b">
             <p>
-            Nigerielles s’engage à innover le monde entrepreneurial féminin nigérien et à rendre possible l’impossible. Nous nous engageons pour une réussite au féminin !! Notre objectif est d’assurer un développement permanent de l’entrepreneuriat féminin en assurant à toutes les femmes des programmes d’accompagnement et des événements pointus et innovants qui répondent constamment à leurs besoins et objectifs.</p>
+            Nigerielles s’engage à innover le monde entrepreneuriat féminin nigérien et à rendre possible l’impossible. Nous nous engageons pour une réussite au féminin !! Notre objectif est d’assurer un développement permanent de l’entrepreneuriat féminin en assurant à toutes les femmes des programmes d’accompagnement et des événements pointus et innovants qui répondent constamment à leurs besoins et objectifs.</p>
           
           </div>
           <div class="block-highlight block-pd-h block-pd-sm">
